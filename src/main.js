635d80ca1cc1b1e8d951f3393410b75ba6d2237a
Nikki Wong <nikki.wong@version1.com>
2018-06-29 15:40:32 +0100

    changing the app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 const electron = require('electron')
-const TurndownService =  require('turndown')
+const TurndownService = require('turndown')
 const fs = require('fs')
 const { promisify } = require('util')
 const readFile = promisify(fs.readFile);
-
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-const Menu = electron.Menu
-
+const mammoth = require('mammoth')
 const countdown = require('./countdown');
 const ipc = electron.ipcMain
 
@@ -16,53 +14,59 @@ let mainWindow
 app.on('ready', _ => {
   mainWindow = new BrowserWindow({
     height: 400,
-    width:400
+    width: 400
   })
 
   mainWindow.loadURL(`file://${__dirname}/countdown.html`)
   mainWindow.on('closed', _ => {
     mainWindow = null
   })
-
-  // const name = electron.app.getName()
-  // const template = [
-  //   {
-  //     label: name,
-  //     submenu: [{
-  //       label: `About ${name}`,
-  //       click: _ => {
-  //         console.log('Clicked About')
-  //       },
-  //       role: 'about'
-  //     }, {
-  //       type: 'separator'
-  //     }, {
-  //       label: 'Quit',
-  //       click: _ => {app.quit()},
-  //       accelerator: 'Cmd+Q'
-  //     }]
-  //   }
-  // ]
-  // const menu = Menu.buildFromTemplate(template)
-  // Menu.setApplicationMenu(menu)
 })
 
-ipc.on('countdown-start', _ => {
-  // (async() => {
-  //   let countdownHtmlData
-  //   try {
-  //     countdownHtmlData = await readFile('src/countdown.html', 'utf8')
-  //       console.log(countdownHtmlData);
-  //   } catch(e){
-  //     console.error(e);
-  //   }
-  //   const turndownService = new TurndownService()
-  //   const markdown = turndownService.turndown(countdownHtmlData)
-  //   console.log(markdown)
-  // })();
+ipc.on('countdown-start', (evt, path, name) => {
+  (async () => {
+    let countdownHtmlData
+    let countdownMHtmlData
+    const dir = '/docs';
+    try {
+      var html
+      mammoth.convertToHtml({ path: path[0] })
+        .then(function (result) {
+          if (!fs.existsSync(directory+dir)) {
+            fs.mkdirSync(directory+dir);
+          }        
+          html = result.value; // The generated HTML
+          var messages = result.messages; // Any messages, such as warnings during conversion
+          fs.writeFile(dirFileName + '.html', html, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        })
+        .done();
+    } catch (e) {
+      console.error(e);
+    }
 
-  console.log(countdownHtmlData)
-  countdown(count => {
-    mainWindow.webContents.send('countdown', count)
-  })
+    // let fileExtension = ""
+    let dirFileName = path[0].replace(/\.doc.$/, "")
+    let name = path[1].replace(/\.doc.$/, "")
+    const directory = path[0].replace(path[1], "")
+
+    countdownHtmlData = await readFile(dirFileName + '.html', 'utf8')
+
+    const turndownService = new TurndownService()
+    const markdown = turndownService.turndown(countdownHtmlData)
+
+    fs.writeFile(directory+dir+"/"+name + '.md', markdown, function (err) {
+      if (err) throw err;
+      console.log('Saved md file!');
+    });
+
+    mainWindow.webContents.send('countdown', "Saved")
+  })();
+
+  // console.log(countdownHtmlData)
+  // countdown(count => {
+  //   mainWindow.webContents.send('countdown', count)
+  // })
 })
--- src/renderer.js
+++ src/renderer.js
@@ -2,9 +2,12 @@ const electron = require('electron')
 const ipc = electron.ipcRenderer
 
 document.getElementById('start').addEventListener('click', _ =>{
-  ipc.send('countdown-start')
+  const path = document.getElementById("myFile").files[0].path;
+  var name = document.getElementById("myFile").files[0].name;
+  ipc.send('countdown-start', [path, name])
 })
 
-ipc.on('countdown', (evt, count) => {
-  document.getElementById('count').innerHTML = count;
+ipc.on('countdown', (evt, markdown) => {
+
+  document.getElementById('count').innerHTML = markdown;
 })
